5e750bc5e1fe0fa01a57d4d46b466422105a933e
DiDinhKhoi <didinhkhoi.tdc2020@gmail.com>
2024-05-06 18:35:49 +0700

    update login san pham

--- 21-training-laravel/routes/web.php
+++ 21-training-laravel/routes/web.php
@@ -17,3 +17,15 @@ Route::get('/', function () {
     return view('welcome');
 });
 Route::get('login', [CustomAuthController::class, 'index'])->name('login');
+
+ // login user :
+ Route::get('/login',[AuthManager::class,'login'])->name('login');
+ Route::post('/login',[AuthManager::class,'loginPost'])->name('login.post');
+ Route::get('/registration',[AuthManager::class,'registration'])->name('registration');
+ Route::post('/registration',[AuthManager::class,'registrationPost'])->name('registration.post');
+ Route::get('/logout',[AuthManager::class,'logout'])->name('logout');
+ //san pham
+ Route::get('/ad/delete_product/{product_id}',[ProductController::class,'delete_product']);
+ //save san pham
+ Route::post('/ad/updateSP/{product_id}',[ProductController::class,'update_product']);
+ Route::post('/ad/timkiemSP',[ProductController::class,'search']);
