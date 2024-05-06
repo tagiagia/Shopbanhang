d4a391c7eb0892a2d76905180db22b80e9ef1428
DiDinhKhoi <didinhkhoi.tdc2020@gmail.com>
2024-05-06 19:19:14 +0700

    login

--- 21-training-laravel/resources/views/login.blade.php
+++ 21-training-laravel/resources/views/login.blade.php
@@ -25,8 +25,8 @@
             <div class="container">
                 <div class="pull-left auto-width-left">
                     <ul class="top-menu menu-beta l-inline">
-                        <li><a href=""><i class="fa fa-home"></i>Thành Phố Hồ Chí Minh</a></li>
-                        <li><a href=""><i class="fa fa-phone"></i>0358449577</a></li>
+                        <li><a href=""><i class="fa fa-home"></i> 90-92 Lê Thị Riêng, Bến Thành, Quận 1</a></li>
+                        <li><a href=""><i class="fa fa-phone"></i> 0163 296 7751</a></li>
                     </ul>
                 </div>
                 <div class="pull-right auto-width-right">
@@ -38,7 +38,7 @@
                 </div>
                 <div class="clearfix"></div>
             </div> <!-- .container -->
-        </div> <!-- .header-topd -->
+        </div> <!-- .header-top -->
         <div class="header-body">
             <div class="container beta-relative">
                 <div class="pull-left">
