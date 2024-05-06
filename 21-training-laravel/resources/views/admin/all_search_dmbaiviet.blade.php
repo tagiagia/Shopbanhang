9ae2f3b816629e54aaad00cd96ecf5ee6da708c4
phamquocan10122004 <phamducan10122004@gmail.com>
2024-05-06 22:08:37 +0700

    bai viet

---
+++ 21-training-laravel/resources/views/admin/all_search_dmbaiviet.blade.php
@@ -0,0 +1,64 @@
+@extends('admin.index')
+@section('admin_content')
+<div class="table-agile-info">
+    <div class="panel panel-default">
+      <div class="panel-heading"> 
+       Danh Mục Tìm Được
+      </div>
+      <div class="row w3-res-tb">
+        <div class="col-sm-5 m-b-xs">
+          <select class="input-sm form-control w-sm inline v-middle">
+            <option value="0">Bulk action</option>
+            <option value="1">Delete selected</option>
+            <option value="2">Bulk edit</option>
+            <option value="3">Export</option>
+          </select>
+          <button class="btn btn-sm btn-default">Apply</button>                
+        </div>
+        <div class="col-sm-4">
+        </div>
+        <div class="col-sm-3">
+        
+        </div>
+      </div>
+      <div class="table-responsive">
+        <table class="table table-striped b-t b-light">
+          <thead>
+            <tr>
+              <th style="width:20px;">
+                <label class="i-checks m-b-none">
+                  <input type="checkbox"><i></i>
+                </label>
+              </th>
+              <th>Tên Bài Viết</th>
+              
+              <th style="width:30px;"></th>
+            </tr>
+          </thead>
+          <tbody>
+            @foreach($search_baiviet as $key =>$pro)
+            <tr>
+              <td><label class="i-checks m-b-none"><input type="checkbox" name="post[]"><i></i></label></td>
+              <td>{{$pro->danhmuc_name}}</td>
+            </tr>     
+            @endforeach   
+          </tbody>                   
+        </table>
+      </div>
+      <footer class="panel-footer">
+        <div class="row">
+          <div class="col-sm-7 text-right text-center-xs">                
+            <ul class="pagination pagination-sm m-t-none m-b-none">
+              <li><a href=""><i class="fa fa-chevron-left"></i></a></li>
+              <li><a href="">1</a></li>
+              <li><a href="">2</a></li>
+              <li><a href="">3</a></li>
+              <li><a href="">4</a></li>
+              <li><a href=""><i class="fa fa-chevron-right"></i></a></li>
+            </ul>
+          </div>
+        </div>
+      </footer>
+    </div>
+  </div>
+ @endsection
