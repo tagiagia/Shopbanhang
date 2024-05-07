c0661c0761f7fb78b18b0fd494b91acf5a875c6f
HoDaoVietThang <vietthang12022003@gmail.com>
2024-05-07 19:48:42 +0700

    thông tin sản phẩm

--- 21-training-laravel/resources/views/Frontend/shop/index_posts.blade.php
+++ 21-training-laravel/resources/views/Frontend/shop/index_posts.blade.php
@@ -23,8 +23,8 @@
             <div class="container">
                 <div class="pull-left auto-width-left">
                     <ul class="top-menu menu-beta l-inline">
-                        <li><a href=""><i class="fa fa-home"></i> 90-92 Lê Thị Riêng, Bến Thành, Quận 1</a></li>
-                        <li><a href=""><i class="fa fa-phone"></i> 0163 296 1234</a></li>
+                        <li><a href=""><i class="fa fa-home"></i> 218 Tô Ngọc Vân Linh Đông Thủ Đức</a></li>
+                        <li><a href=""><i class="fa fa-phone"></i> 0163 296 7751</a></li>
                     </ul>
                 </div>
                 <div class="pull-right auto-width-right">
