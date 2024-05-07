7164486dbb6b47d5413876e9c93f76110a76e095
DiDinhKhoi <didinhkhoi.tdc2020@gmail.com>
2024-05-07 19:39:27 +0700

    update

--- 21-training-laravel/resources/views/layout.blade.php
+++ 21-training-laravel/resources/views/layout.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" >
 
-    <title>Document</title>
+    <title>Shop Bán Hàng</title>
 </head>
 <body>
     @yield('content')
