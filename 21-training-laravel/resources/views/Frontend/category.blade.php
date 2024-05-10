82e6b80df1a87b08662a4bef05b4b844b6109e12
DiDinhKhoi <didinhkhoi.tdc2020@gmail.com>
2024-05-10 13:07:00 +0700

    update

--- 21-training-laravel/resources/views/Frontend/category.blade.php
+++ 21-training-laravel/resources/views/Frontend/category.blade.php
@@ -4,7 +4,7 @@
 	<base href="{{asset('/')}}">
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Levents - Products</title>
+	<title>Shop Bán Hàng - Products</title>
 	<link href='http://fonts.googleapis.com/css?family=Dosis:300,400' rel='stylesheet' type='text/css'>
 	<link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300' rel='stylesheet' type='text/css'>
 	<link rel="stylesheet" href="http://netdna.bootstrapcdn.com/bootstrap/3.1.0/css/bootstrap.min.css">
--- 21-training-laravel/resources/views/Frontend/index.blade.php
+++ 21-training-laravel/resources/views/Frontend/index.blade.php
@@ -11,7 +11,7 @@
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Levents </title>
+	<title>Shop Bán Hàng </title>
 	<link href='http://fonts.googleapis.com/css?family=Dosis:300,400' rel='stylesheet' type='text/css'>
 	<link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300' rel='stylesheet' type='text/css'>
 	<link rel="stylesheet" href="http://netdna.bootstrapcdn.com/bootstrap/3.1.0/css/bootstrap.min.css">
