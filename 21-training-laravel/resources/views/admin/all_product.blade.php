b557b5cff2b7775840ebe9e34cbf0173cba1d647
HoDaoVietThang <vietthang12022003@gmail.com>
2024-05-16 15:33:10 +0700

    fix liệt kê sản phẩm

--- 21-training-laravel/resources/views/admin/all_product.blade.php
+++ 21-training-laravel/resources/views/admin/all_product.blade.php
@@ -3,7 +3,7 @@
 <div class="table-agile-info">
     <div class="panel panel-default">
       <div class="panel-heading"> 
-       Liệt Sản Phẩm
+       Liệt Kê Sản Phẩm
       </div>
       <div class="row w3-res-tb">
         <div class="col-sm-5 m-b-xs">
